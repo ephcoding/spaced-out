9bf3d2f4ec74a63190c4446d3394a35c3fd81b21
Ephraim <ephraim@modevx.com>
2022-07-05 07:49:47 -0400

    add test Footer links

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,17 +1,26 @@
 import { Footer as DUIFooter } from "react-daisyui";
-import { Link } from "react-router-dom";
+import { ExtLink } from "./ExtLink";
 
 export const Footer = () => {
   return (
     <DUIFooter>
       <div>
         <DUIFooter.Title>NASA +</DUIFooter.Title>
+        <ExtLink href="#">Test link</ExtLink>
+        <ExtLink href="#">Test link</ExtLink>
+        <ExtLink href="#">Test link</ExtLink>
       </div>
       <div>
         <DUIFooter.Title>Developers</DUIFooter.Title>
+        <ExtLink href="#">Test link</ExtLink>
+        <ExtLink href="#">Test link</ExtLink>
+        <ExtLink href="#">Test link</ExtLink>
       </div>
       <div>
         <DUIFooter.Title>About Ephraim</DUIFooter.Title>
+        <ExtLink href="#">Test link</ExtLink>
+        <ExtLink href="#">Test link</ExtLink>
+        <ExtLink href="#">Test link</ExtLink>
       </div>
     </DUIFooter>
   );
