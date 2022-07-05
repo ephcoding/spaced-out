cfaa7b65658a46bb23eb478d41c7594f1338fc3a
Ephraim <ephraim@modevx.com>
2022-07-05 07:09:16 -0400

    rough-in init react router

--- src/App.js
+++ src/App.js
@@ -1,41 +1,10 @@
-import React from "react"; // Suspense, // useTransition, // useState, // useMemo, // useEffect, // useDeferredValue, // useCallback,
-import { Button, Card, Collapse } from "react-daisyui";
-
-import { useQuery } from "react-query";
+import { Routes, Route, Link } from "react-router-dom";
+import { APODScreen } from "./pages/ApodScreen";
 
 export default function App() {
-  const { data, error, isLoading } = useQuery(["APOD", "today"], async () => {
-    const raw = await fetch(
-      `https://api.nasa.gov/planetary/apod?api_key=${process.env.REACT_APP_NASA_KEY}`
-    );
-    return await raw.json();
-  });
-
-  if (error) {
-    return <p>{error.message}</p>;
-  }
-  if (isLoading) {
-    return <p>Loading...</p>;
-  }
-
-  const keys = Object.keys(data).map((key) => <p key={key}>{key}</p>);
-
   return (
-    <div className="min-h-screen bg-stone-800 text-stone-100">
-      <div>{keys}</div>
-      <Card bordered={true} compact={true} className="w-[50vw]">
-        <Card.Image src={data.url} alt="NASA Photo of the Day" />
-        <Card.Title className="text-3xl text-red-500">{data.title}</Card.Title>
-        <Card.Body>
-          <Collapse checkbox={true}>
-            <Collapse.Title>{`${data.explanation.slice(
-              0,
-              40
-            )}...`}</Collapse.Title>
-            <Collapse.Content>{data.explanation}</Collapse.Content>
-          </Collapse>
-        </Card.Body>
-      </Card>
-    </div>
+    <Routes>
+      <Route path="/" element={<APODScreen />} />
+    </Routes>
   );
 }
